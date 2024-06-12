f8e159d1dee2191720345e40d9f851149b372158
chutoan2306 <truongvanchutoan1999@gmail.com>
2024-06-12 11:52:28 +0700

    chore: merge code

--- src/containers/experience/index.tsx
+++ src/containers/experience/index.tsx
@@ -10,23 +10,25 @@ export default function Experience() {
     >
       {EXPERIENCES.map((item: any, index: number) => (
         <div className="rounded-xl bg-white" key={index} data-aos="zoom-in-up">
-          <div className="p-[25px] md:p-[2rem]  w-full items-start justify-start flex-col md:flex flex gap-[1rem] md:gap-10 relative lt:flex-row lt:w-[56rem]">
+          <div className="px-[1.563rem] pb-[1.563rem] md:px-[1.75rem] md:pt-[1.75rem] md:pb-[1rem] w-full items-start justify-start flex-col md:flex flex gap-[1rem] md:gap-10 relative lt:flex-row lt:w-[56rem]">
             <div className="flex w-[7.125rem] flex-col items-start gap-3 shrink-0">
               <p className=" text-primary text-base not-italic font-semibold leading-6 flex-col sm:flex">{item.label}</p>
             </div>
             <div className="flex-1">
               <h3 className="self-stretch text-primary text-xl not-italic font-semibold">{item.position}</h3>
-              <ul className="flex flex-col items-start gap-1 self-stretch list-disc ml-[0.938rem] pl-[0.625rem]">
-                {item.contents.map((content: string, index: number) => (
-                  <li className="self-stretch text-gray-600 text-lg not-italic font-normal leading-6" key={index}>
-                    {content}
-                  </li>
-                ))}
-              </ul>
             </div>
             <div className="lt:w-[11.25rem] absolute  right-[2.5rem] lt:sticky lt:flex lt:justify-end">
               <span className="text-primary text-right text-base not-italic font-normal">{item.time}</span>
             </div>
+          </div>
+          <div className="px-[1.563rem] pb-[1.563rem] md:px-[2rem] md:pb-[2rem] w-full lt:w-[56rem] items-start justify-start flex-col md:flex flex gap-[1rem] md:gap-10 relative lt:flex-row ">
+            <ul className="flex flex-col items-start gap-1 self-stretch list-disc ml-[0.938rem] pl-[0.625rem]">
+              {item.contents.map((content: string, index: number) => (
+                <li className="self-stretch text-gray-600 text-lg not-italic font-normal leading-6" key={index}>
+                  {content}
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
       ))}
