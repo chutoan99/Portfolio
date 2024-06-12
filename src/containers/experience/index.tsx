88b75d85a3ad3db28caa006d879986bedec8e38f
chutoan2306 <truongvanchutoan1999@gmail.com>
2024-06-12 13:34:56 +0700

    chore: merge code

--- src/containers/experience/index.tsx
+++ src/containers/experience/index.tsx
@@ -1,4 +1,3 @@
-import { EXPERIENCES } from '../../shared/contants';
 import 'aos/dist/aos.css';
 
 export default function Experience() {
@@ -24,28 +23,73 @@ export default function Experience() {
                     </div>
                     <div className="ml-14 flex flex-col gap-[3px]">
                       <div className="text-slate-500">
-                        <span className="text-description font-bold">{EXPERIENCES[0].label}</span> {EXPERIENCES[0].position}
+                        <span className="text-description font-bold">Y TE SO GIA AN (A Company under HOA LÂM Group):</span> Front End Developer
                       </div>
-                      <time className="text-primary text-left text-base not-italic font-normal md:w-21">{EXPERIENCES[0].time}</time>
+                      <time className="text-primary text-left text-base not-italic font-normal md:w-21">Aug 2022 – May 2024</time>
                     </div>
                   </div>
                   <div className="ml-14 md:ml-[4rem] flex flex-col gap-[10px] w-full pr-[3rem]">
                     <div className="bg-white p-4 rounded-[6px] border border-slate-200 text-slate-500 shadow" data-aos="zoom-in-up">
-                      <ul className="flex flex-col items-start gap-1 self-stretch">
-                        {EXPERIENCES[0].contents.map((content: string, index: number) => (
-                          <li className="self-stretch text-gray-600 text-lg not-italic font-normal leading-6" key={index}>
-                            {content}
-                          </li>
-                        ))}
+                      <ul className="flex flex-col items-start gap-[5px] self-stretch">
+                        <li className="self-stretch text-gray-600 text-lg not-italic font-normal leading-6">Develop & implement projects for CIH International Hospital and Gia An 155 Hospital:</li>
+                        <li className="self-stretch text-gray-600 text-lg not-italic font-normal leading-6 pl-[10px]">
+                          <span className="text-description font-bold">HRM: Human resource management.</span> Handle Electronic Medical Records, storing comprehensive medical data for each patient,
+                          including medical history, diagnosis, and treatment.
+                        </li>
+                        <li className="self-stretch text-gray-600 text-lg not-italic font-normal leading-6 pl-[10px]">
+                          <span className="text-description font-bold">ERP: Enterprise resource planning.</span> Manage all employee information with multiple modules, including personnel, leave,
+                          insurance, salary & personal income tax, and labor equipment.
+                        </li>
+                        <li className="self-stretch text-gray-600 text-lg not-italic font-normal leading-6 pl-[10px]">
+                          <span className="text-description font-bold">EMR: Electronic medical record.</span> Handle Electronic Medical Records, storing comprehensive medical data for each patient,
+                          including medical history, diagnosis, and treatment.
+                        </li>
+                        <li className="self-stretch text-gray-600 text-lg not-italic font-normal leading-6">Team size: 18 and the main tasks:</li>
+                        <li className="self-stretch text-gray-600 text-lg not-italic font-normal leading-6 pl-[10px]">
+                          + Work with TypeScript, Angular, Rxjs, TailwindCss and DevExpress, SQL Server…
+                        </li>
+                        <li className="self-stretch text-gray-600 text-lg not-italic font-normal leading-6 pl-[10px]">
+                          + Create an API using ASP.NET in C#(C Sharp), and Document with Swagger for the FE.
+                        </li>
+                        <li className="self-stretch text-gray-600 text-lg not-italic font-normal leading-6 pl-[10px]">
+                          + Collaborate with BA and QC to ensure alignment with business team objectives and requirements.
+                        </li>
+                        <li className="self-stretch text-gray-600 text-lg not-italic font-normal leading-6 pl-[10px]">+ Create report templates use 'report designer DevExpress'</li>
+                        <li className="self-stretch text-gray-600 text-lg not-italic font-normal leading-6 pl-[10px]">
+                          + Participate in project development meets, contribute innovative solutions, and address potential risks.
+                        </li>
+                        <li className="self-stretch text-gray-600 text-lg not-italic font-normal leading-6 pl-[10px]">+ Work with the agile scrum methodology.</li>
                       </ul>
                     </div>
                     <div className="bg-white p-4 rounded-[6px] border border-slate-200 text-slate-500 shadow" data-aos="zoom-in-up">
-                      <ul className="flex flex-col items-start gap-1 self-stretch">
-                        {EXPERIENCES[0].contents2?.map((content: string, index: number) => (
-                          <li className="self-stretch text-gray-600 text-lg not-italic font-normal leading-6" key={index}>
-                            {content}
-                          </li>
-                        ))}
+                      <ul className="flex flex-col items-start gap-[5px] self-stretch">
+                        <li className="self-stretch text-gray-600 text-lg not-italic font-normal leading-6">Developed outsource projects:</li>
+                        <li className="self-stretch text-gray-600 text-lg not-italic font-normal leading-6 pl-[10px]">
+                          <span className="text-description font-bold">iOki: Objectives & Key Results.</span> Manage and scor OKR of employees every quarter.
+                        </li>
+                        <li className="self-stretch text-gray-600 text-lg not-italic font-normal leading-6">Team size: 20 and the main tasks:</li>
+                        <li className="self-stretch text-gray-600 text-lg not-italic font-normal leading-6 pl-[10px]">+ Work with TypeScript, Angular, Jquery, Rxjs, Ngrx, Scss, Bootstrap…</li>
+                        <li className="self-stretch text-gray-600 text-lg not-italic font-normal leading-6 pl-[10px]">
+                          + Ensure precise conversion of Figma designs to web format with pixel-perfect accuracy and responsiveness.
+                        </li>
+                        <li className="self-stretch text-gray-600 text-lg not-italic font-normal leading-6 pl-[10px]">
+                          <span className="text-description font-bold">iSecurity: </span>The system consolidates all security features within an area, integrates facial recognition, records, camera
+                          management, identification of passenger vehicles upon entry and exit, suggests intrusive behavior, object track on maps…
+                        </li>
+                        <li className="self-stretch text-gray-600 text-lg not-italic font-normal leading-6">Team size: 10 and the main tasks:</li>
+                        <li className="self-stretch text-gray-600 text-lg not-italic font-normal leading-6 pl-[10px]">+ Work with AngularJs, Javascript, Fabricjs, Bootstrap…</li>
+                        <li className="self-stretch text-gray-600 text-lg not-italic font-normal leading-6 pl-[10px]">
+                          + Responsible for maintain, develop new features, and manage source code versions of the company previously.
+                        </li>
+                        <li className="self-stretch text-gray-600 text-lg not-italic font-normal leading-6 pl-[10px]">
+                          + Support and guidance to students during their internship on various projects.
+                        </li>
+                        <li className="self-stretch text-gray-600 text-lg not-italic font-normal leading-6 pl-[10px]">
+                          <span className="text-description font-bold">iWater: </span>Main features include management water usage history, payment records, employee…
+                        </li>
+                        <li className="self-stretch text-gray-600 text-lg not-italic font-normal leading-6">Team size: 8 and the main tasks:</li>
+                        <li className="self-stretch text-gray-600 text-lg not-italic font-normal leading-6 pl-[10px]">+ Work with TypeScript, React, Redux, Material UI…</li>
+                        <li className="self-stretch text-gray-600 text-lg not-italic font-normal leading-6 pl-[10px]">+ Read and comprehend BA documents to align with project requirements.</li>
                       </ul>
                     </div>
                   </div>
@@ -61,19 +105,18 @@ export default function Experience() {
                     </div>
                     <div className="ml-14 flex flex-col gap-[3px]">
                       <div className="text-slate-500">
-                        <span className="text-description font-bold">{EXPERIENCES[1].label}</span> {EXPERIENCES[1].position}
+                        <span className="text-description font-bold">STECH VIET NAM:</span> May 2022 - Jul 2022
                       </div>
-                      <time className="text-primary text-left text-base not-italic font-normal md:w-21">{EXPERIENCES[0].time}</time>
+                      <time className="text-primary text-left text-base not-italic font-normal md:w-21">Intern Backend Developer</time>
                     </div>
                   </div>
                   <div className="ml-14 md:ml-[4rem] flex flex-col gap-[10px] w-full pr-[3rem]">
                     <div className="bg-white p-4 rounded-[6px] border border-slate-200 text-slate-500 shadow" data-aos="zoom-in-up">
                       <ul className="flex flex-col items-start gap-1 self-stretch">
-                        {EXPERIENCES[1].contents.map((content: string, index: number) => (
-                          <li className="self-stretch text-gray-600 text-lg not-italic font-normal leading-6" key={index}>
-                            {content}
-                          </li>
-                        ))}
+                        <li className="self-stretch text-gray-600 text-lg not-italic font-normal leading-6">Develop web application 'Social Network for Blockchain':</li>
+                        <li className="self-stretch text-gray-600 text-lg not-italic font-normal leading-6">Team size: 20 and the main tasks:</li>
+                        <li className="self-stretch text-gray-600 text-lg not-italic font-normal leading-6 pl-[10px]">+ Work with Nodejs, Typescript, Sequelize, MySql, RESTfull api…</li>
+                        <li className="self-stretch text-gray-600 text-lg not-italic font-normal leading-6 pl-[10px]">+ Create Api (accord to restful standard) and integrate with Fe.</li>
                       </ul>
                     </div>
                   </div>
