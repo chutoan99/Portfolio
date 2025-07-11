a638a947facca1f64ed480585e593baa8073e35f
toantruong2 <toan.truong@realestatedoc.co>
2025-07-11 20:24:53 +0700

    chore: merge code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,19 +10,53 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <title>Chu Toan | Frontend Developer Portfolio</title>
         <link rel="shortcut icon" href="/rounded-avatar.png" type="image/x-icon" />
 
-        <title>{`Portfolio`}</title>
+        {/* Primary Meta Tags */}
+        <meta name="title" content="Chu Toan | Frontend Developer Portfolio" />
+        <meta name="description" content="Portfolio Trương Văn Chu Toàn - Frontend Developer - BackEnd Developer" />
+        <meta name="keywords" content="Portfolio Trương Văn Chu Toàn - Frontend Developer - BackEnd Developer" />
+        <meta
+          name="keywords"
+          content="
+            Trương Văn Chu Toàn,
+            Chu Toan,
+            Frontend Developer,
+            Backend Developer,
+            Fullstack Developer,
+            Web Developer Portfolio,
+            Software Engineer Vietnam,
+            Full Stack Web Developer,
+            Personal Website Design,
+            Developer Portfolio,
+            Best Portfolio Websites,
+            Beautiful Portfolio,
+          "
+        />
 
-        <meta name="description" content="Chu toan Portfolio" />
+        <meta name="author" content="Trương Văn Chu Toàn" />
+        <meta name="robots" content="index, follow" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
 
+        {/* Open Graph / Facebook */}
         <meta property="og:type" content="website" />
-        <meta property="og:title" content="Chu Toan dev Portfolio" />
-        <meta property="og:description" content="Trương Văn Chu Toàn" />
+        <meta property="og:url" content="https://portfolio-chutoan.vercel.app/" />
+        <meta property="og:title" content="Chu Toan | Frontend Developer Portfolio" />
+        <meta property="og:description" content="Portfolio Trương Văn Chu Toàn - Frontend Developer - BackEnd Developer" />
+        <meta property="og:image" content="https://yourdomain.com/preview-image.png" />
 
+        {/* Twitter */}
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:title" content="Chu Toan dev Portfolio" />
+        <meta property="twitter:url" content="https://portfolio-chutoan.vercel.app/" />
+        <meta property="twitter:title" content="Chu Toan | Frontend Developer Portfolio" />
+        <meta property="twitter:description" content="Portfolio Trương Văn Chu Toàn - Frontend Developer - BackEnd Developer" />
+        <meta property="twitter:image" content="https://yourdomain.com/preview-image.png" />
+
+        {/* Canonical URL */}
+        <link rel="canonical" href="https://portfolio-chutoan.vercel.app/" />
       </Head>
+
       <LazyMotion features={domAnimation}>
         <Component {...pageProps} />
       </LazyMotion>
